bcf85a9769bc98144252cb8748624a26ee92f58f
balthaB <balthazar.baillat@epfl.ch>
2024-02-24 13:30:20 +0100

    implement and comment TileDecks class

--- src/ch/epfl/chacun/TileDecks.java
+++ src/ch/epfl/chacun/TileDecks.java
@@ -1,8 +1,11 @@
 package ch.epfl.chacun;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
+ * Represents the three decks of tiles.
+ *
  * @param startTiles
  * @param normalTiles
  * @param menhirTiles
@@ -11,30 +14,105 @@ import java.util.List;
  */
 public record TileDecks(List<Tile> startTiles, List<Tile> normalTiles, List<Tile> menhirTiles) {
 
+    /**
+     * Makes a defensive copy of the tile lists.
+     *
+     * @param startTiles
+     * @param normalTiles
+     * @param menhirTiles
+     */
     public TileDecks {
         startTiles = List.copyOf(startTiles);
         normalTiles = List.copyOf(normalTiles);
         menhirTiles = List.copyOf(menhirTiles);
     }
 
+    /**
+     * Returns the size of the deck containing tiles of a given kind.
+     *
+     * @param kind the kind of tile
+     * @return the size of the deck o a given kind
+     */
     public int deckSize(Tile.Kind kind) {
-        int deckSize = switch (kind) {
+        return switch (kind) {
             case START -> startTiles.size();
             case NORMAL -> normalTiles.size();
             case MENHIR -> menhirTiles.size();
         };
-        return deckSize;
     }
 
+    /**
+     * Returns the tile at the top of the deck containing tiles of the given kind,
+     * or null if the deck is empty.
+     *
+     * @param kind the kind of tile
+     * @return the tile at the top of the deck containing tiles of the given kind,
+     * or null if the deck is empty
+     */
     public Tile topTile(Tile.Kind kind) {
-        Tile topTile = switch (kind) {
-            case START -> startTiles.getFirst();
-            case NORMAL -> normalTiles.getFirst();
-            case MENHIR -> menhirTiles.getFirst();
-        };
-        return topTile;
+        if (this.deckSize(kind) <= 0) {
+            return null;
+        } else {
+            return switch (kind) {
+                case START -> startTiles.getFirst();
+                case NORMAL -> normalTiles.getFirst();
+                case MENHIR -> menhirTiles.getFirst();
+            };
+        }
     }
 
+    /**
+     * Returns a new triplet of decks after removing from the receiver triplet the top tile of the deck
+     * containing tiles of a given kind.
+     *
+     * @param kind the kind of tile
+     * @return a new triplet of decks after removing from the receiver triplet the top tile of the deck
+     *         containing tiles of a given kind
+     * @throws IllegalArgumentException if the receiver deck of the given tile kind is empty
+     */
     public TileDecks withTopTileDrawn(Tile.Kind kind) {
+        if (this.deckSize(kind) <= 0) {
+            throw new IllegalArgumentException("The deck of the given tile is empty");
+        }
+        switch (kind) {
+            case START -> this.startTiles.remove(0);
+            case NORMAL -> this.normalTiles.remove(0);
+            case MENHIR -> this.menhirTiles.remove(0);
+        }
+        return this;
+    }
+
+    /**
+     * Returns a new triplet of decks after testing on the receiver deck containing the given tile kind a
+     * given predicate using testPredicateOnDeck.
+     *
+     * @param kind the kind of tile
+     * @param predicate the predicate to test
+     * @return a new triplet of decks after testing a given predicate on the receiver triplet
+     */
+    public TileDecks withTopTileDrawnUntil(Tile.Kind kind, Predicate<Tile> predicate) {
+        switch (kind) {
+            case START -> testPredicateOnDeck(this.startTiles, predicate);
+            case NORMAL -> testPredicateOnDeck(this.normalTiles, predicate);
+            case MENHIR -> testPredicateOnDeck(this.menhirTiles, predicate);
+        }
+        return this;
+    }
+
+    /**
+     * Returns a new deck of tiles after removing the tiles that does not respect a given predicate.
+     *
+     * @param deck the deck of tiles
+     * @param predicate the predicate to check
+     * @return a new deck of tiles after removing the tiles that does not respect a given predicate
+     */
+    private List<Tile> testPredicateOnDeck(List<Tile> deck, Predicate predicate) {
+        List<Tile> newdeck = deck;
+        for (int i = 0; i < newdeck.size(); ++i) {
+            if (!predicate.test(newdeck.get(i))) {
+                newdeck.remove(i);
+            }
+        }
+        return newdeck;
     }
 }
