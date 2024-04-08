50ad63146edaa2459e4377b3d63ab1597f86700b
Max <maxence1604.minecraft@gmail.com>
2024-04-08 14:55:31 +0200

    Add comments to step 7

--- src/ch/epfl/chacun/gui/ColorMap.java
+++ src/ch/epfl/chacun/gui/ColorMap.java
@@ -3,14 +3,31 @@ package ch.epfl.chacun.gui;
 import ch.epfl.chacun.PlayerColor;
 import javafx.scene.paint.Color;
 
+/**
+ * Helper class to map player colors to JavaFX colors.
+ *
+ * @author Maxence Espagnet (sciper: 372808)
+ * @author Balthazar Baillat (sciper: 373420)
+ */
 public final class ColorMap {
 
-    private static final double OPACITY_FACTOR = .6;
+    /**
+     * The opacity factor to use for the stroke color.
+     */
+    private static final double STROKE_OPACITY_FACTOR = .6;
 
-    // Non-instantiable
+    /**
+     * Non-instantiable class constructor
+     */
     private ColorMap() {
     }
 
+    /**
+     * Returns the fill color for the given player color.
+     *
+     * @param playerColor the player color
+     * @return the fill color
+     */
     public static Color fillColor(PlayerColor playerColor) {
         return switch (playerColor) {
             case RED -> Color.RED;
@@ -21,9 +38,18 @@ public final class ColorMap {
         };
     }
 
+    /**
+     * Returns the stroke color for the given player color.
+     *
+     * @param playerColor the player color
+     * @return the stroke color
+     */
     public static Color strokeColor(PlayerColor playerColor) {
+        // For better contrast with light colors, use a darker stroke color
         if (playerColor == PlayerColor.YELLOW || playerColor == PlayerColor.GREEN)
-            return fillColor(playerColor).deriveColor(0, 1, 1, OPACITY_FACTOR);
+            return fillColor(playerColor)
+                    .deriveColor(0, 1, 1, STROKE_OPACITY_FACTOR);
+        // For dark colors, use a lighter stroke color
         return Color.WHITE;
     }
 
