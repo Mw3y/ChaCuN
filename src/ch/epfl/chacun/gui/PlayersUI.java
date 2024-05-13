8bab265a569cbe66aaa9621e138a0b130d950fc8
Max <maxence1604.minecraft@gmail.com>
2024-05-13 11:28:52 +0200

    Enhancements suggested by M. Schinz

--- src/ch/epfl/chacun/gui/PlayersUI.java
+++ src/ch/epfl/chacun/gui/PlayersUI.java
@@ -57,54 +57,52 @@ public final class PlayersUI {
                 gameStateO.map((GameState gameState) -> gameState.messageBoard().points());
 
         // Create the UI for each player
-        for (PlayerColor playerColor : PlayerColor.ALL) {
+        for (PlayerColor playerColor : gameStateO.getValue().players()) {
             String playerName = textMaker.playerName(playerColor);
 
-            if (playerName != null) {
-                TextFlow textFlow = new TextFlow();
-                Text pointsText = new Text();
-                HBox occupantsBox = new HBox();
+            TextFlow textFlow = new TextFlow();
+            Text pointsText = new Text();
+            HBox occupantsBox = new HBox();
 
-                // Dynamically update the points text
-                ObservableValue<String> pointsTextO = pointsO.map(points -> {
-                    int pointsValue = points.getOrDefault(playerColor, 0);
-                    return STR." \{playerName} : \{textMaker.points(pointsValue)}\n";
-                });
-                // Show which player is currently playing
-                currentPlayerO.addListener((_, _, currentPlayer) -> {
-                    textFlow.getStyleClass().remove("current");
-                    if (currentPlayer == playerColor)
-                        textFlow.getStyleClass().add("current");
-                });
+            // Dynamically update the points text
+            ObservableValue<String> pointsTextO = pointsO.map(points -> {
+                int pointsValue = points.getOrDefault(playerColor, 0);
+                return STR." \{playerName} : \{textMaker.points(pointsValue)}\n";
+            });
+            // Show which player is currently playing
+            currentPlayerO.addListener((_, _, currentPlayer) -> {
+                textFlow.getStyleClass().remove("current");
+                if (currentPlayer == playerColor)
+                    textFlow.getStyleClass().add("current");
+            });
 
-                // Add the player's name and points to the UI
-                textFlow.getChildren().add(new Circle(5, ColorMap.fillColor(playerColor)));
-                pointsText.textProperty().bind(pointsTextO);
-                textFlow.getChildren().add(pointsText);
-                textFlow.getStyleClass().add("player");
-                // Add the player's occupants to the UI
-                for (int i = Occupant.Kind.values().length - 1; i >= 0; --i) {
-                    Occupant.Kind kind = Occupant.Kind.values()[i];
-                    HBox box = new HBox();
-                    for (int j = 0; j < Occupant.occupantsCount(kind); ++j) {
-                        Node icon = Icon.newFor(playerColor, kind);
-                        // Make the opacity of the icon dependent on the number of free occupants
-                        int occupantId = j;
-                        ObservableValue<Float> opacityO = gameStateO.map(gameState -> {
-                            int hutsCount = gameState.freeOccupantsCount(playerColor, kind);
-                            return hutsCount > occupantId ? DEFAULT_OCCUPANT_OPACITY : OCCUPANT_USED_OPACITY;
-                        });
-                        icon.opacityProperty().bind(opacityO);
-                        box.getChildren().add(icon);
-                    }
-                    occupantsBox.getChildren().add(box);
+            // Add the player's name and points to the UI
+            textFlow.getChildren().add(new Circle(5, ColorMap.fillColor(playerColor)));
+            pointsText.textProperty().bind(pointsTextO);
+            textFlow.getChildren().add(pointsText);
+            textFlow.getStyleClass().add("player");
+            // Add the player's occupants to the UI
+            for (int i = Occupant.Kind.values().length - 1; i >= 0; --i) {
+                Occupant.Kind kind = Occupant.Kind.values()[i];
+                HBox box = new HBox();
+                for (int j = 0; j < Occupant.occupantsCount(kind); ++j) {
+                    Node icon = Icon.newFor(playerColor, kind);
+                    // Make the opacity of the icon dependent on the number of free occupants
+                    int occupantId = j;
+                    ObservableValue<Float> opacityO = gameStateO.map(gameState -> {
+                        int hutsCount = gameState.freeOccupantsCount(playerColor, kind);
+                        return hutsCount > occupantId ? DEFAULT_OCCUPANT_OPACITY : OCCUPANT_USED_OPACITY;
+                    });
+                    icon.opacityProperty().bind(opacityO);
+                    box.getChildren().add(icon);
                 }
-
-                occupantsBox.setSpacing(10);
-                // Add everything to the UI
-                textFlow.getChildren().add(occupantsBox);
-                container.getChildren().add(textFlow);
+                occupantsBox.getChildren().add(box);
             }
+
+            occupantsBox.setSpacing(10);
+            // Add everything to the UI
+            textFlow.getChildren().add(occupantsBox);
+            container.getChildren().add(textFlow);
         }
         return container;
     }
